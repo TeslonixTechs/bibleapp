3f4a3e2d258668a5aac5628a11c63c0ec73814c2
sam-spec-ops <samueloni0987@gmail.com>
2024-02-21 09:17:17 +0100

    Bible App

--- src/component/BiblePage.js
+++ src/component/BiblePage.js
@@ -12,7 +12,7 @@ const BiblePage = () => {
   }
   return (
     <div className='flex justify-center h-screen w-screen items-center bg-gradient-to-t from-zinc-950 to-green-950'>
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-5'>
             <div onClick={gotobooks} className="flex active:text-zinc-700 gap-4 text-2xl items-center text-amber-600 font-bold"><div className="bg-amber-500 text-white text-2xl h-10 w-10 rounded-full flex justify-center items-center"><span className="fas fa-arrow-right text-green-950 font-bold text-3xl"></span></div><div>Bible</div></div>
             <div onClick={gotostudy} className="flex active:text-zinc-700 gap-4 text-2xl items-center text-cyan-400 font-bold"><div className="bg-cyan-500 text-white text-2xl h-10 w-10 rounded-full flex justify-center items-center"><span className="fas fa-arrow-right text-green-950 font-bold text-3xl"></span></div><div>Bible Study</div></div>
             <div className="flex active:text-zinc-700 gap-4 text-2xl items-center text-amber-600 font-bold"><div className="bg-amber-500 text-white text-2xl h-10 w-10 rounded-full flex justify-center items-center"><span className="fas fa-arrow-right text-green-950 font-bold text-3xl"></span></div><div>Daily Devotional</div></div>
